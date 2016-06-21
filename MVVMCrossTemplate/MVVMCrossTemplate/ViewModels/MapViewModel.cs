c46556b2d0124b0a9d842cfe4d413f48ca446293
James Sturtevant <jstur@microsoft.com>
2016-06-21 15:46:32 -0400

    added back button

--- MVVMCrossTemplate/MVVMCrossTemplate/ViewModels/MapViewModel.cs
+++ MVVMCrossTemplate/MVVMCrossTemplate/ViewModels/MapViewModel.cs
@@ -1,8 +1,16 @@
 ﻿using MvvmCross.Core.ViewModels;
+using System.Windows.Input;
 
 namespace MVVMCrossTemplate.ViewModels
 {
     public class MapViewModel : MvxViewModel
     {
+        public ICommand BackCommand
+        {
+            get
+            {
+                return new MvxCommand(() => Close(this));
+            }
+        }
     }
 }
