7c972e2c6e421aa16acf0cde8929ef14212c31d7
James Sturtevant <jstur@microsoft.com>
2016-06-21 14:06:33 -0400

    Setup UWP to use MVVMCross

--- MVVMCrossTemplate/MVVMCrossTemplate.UWP/App.xaml.cs
+++ MVVMCrossTemplate/MVVMCrossTemplate.UWP/App.xaml.cs
@@ -14,6 +14,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using MvvmCross.Core;
+using MvvmCross.Platform;
 
 namespace MVVMCrossTemplate.UWP
 {
@@ -72,7 +74,12 @@ namespace MVVMCrossTemplate.UWP
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+
+                    var setup = new Setup(rootFrame);
+                    setup.Initialize();
+
+                    var start = Mvx.Resolve<MvvmCross.Core.ViewModels.IMvxAppStart>();
+                    start.Start();
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
