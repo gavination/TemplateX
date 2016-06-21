67c45bcf2ad8b70288ae9d6b52df5bc348501bf1
Gavin Bauman <gavinbauman1322@gmail.com>
2016-06-21 13:09:14 -0400

    Initial Commit

---
+++ MVVMCrossTemplate/MVVMCrossTemplate.WinPhone/Views/FirstView.xaml.cs
@@ -0,0 +1,12 @@
+using MvvmCross.WindowsCommon.Views;
+
+namespace MVVMCrossTemplate.WinPhone.Views
+{
+    public sealed partial class FirstView : MvxWindowsPage
+    {
+        public FirstView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
