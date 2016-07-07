fa715325528e536048a03ab752321552cf03606c
James Sturtevant <jsturtevant@gmail.com>
2016-07-07 13:29:18 -0400

    added authorization to end point

--- MVVMCrossTemplate.Web/Controllers/DogsController.cs
+++ MVVMCrossTemplate.Web/Controllers/DogsController.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
 
 namespace MVVMCrossTemplate.Web.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     public class DogsController : Controller
     {
