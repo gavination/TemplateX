384e8a0ddb71164a6bb70810b014dd582eab384a
James Sturtevant <jsturtevant@gmail.com>
2016-07-13 23:58:34 -0400

    got maps working on emulator.

--- MVVMCrossTemplate/MVVMCrossTemplate.Droid/Views/MapView.cs
+++ MVVMCrossTemplate/MVVMCrossTemplate.Droid/Views/MapView.cs
@@ -1,16 +1,21 @@
 using Android.App;
 using Android.OS;
+using MvvmCross.Droid.FullFragging.Caching;
+using MvvmCross.Droid.FullFragging.Fragments;
 using MvvmCross.Droid.Views;
+using MVVMCrossTemplate.ViewModels;
 
 namespace MVVMCrossTemplate.Droid.Views
 {
     [Activity(Label = "Map")]
-    public class MapView : MvxActivity
+    public class MapView : MvxCachingFragmentActivity<MapViewModel>
     {
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.MapView);
+
+           
         }
     }
 }
